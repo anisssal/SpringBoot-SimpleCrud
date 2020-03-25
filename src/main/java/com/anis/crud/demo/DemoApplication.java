1b6752d16220480ddaeefea03088f676051974a4
anisssal <anis.althiif@gmail.com>
2020-03-25 11:15:40 +0700

    initial commit

---
+++ src/main/java/com/anis/crud/demo/DemoApplication.java
@@ -0,0 +1,17 @@
+package com.anis.crud.demo;
+
+import com.sun.glass.ui.Application;
+import org.apache.catalina.Context;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+
+	}
+
+}
